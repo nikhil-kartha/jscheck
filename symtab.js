d2682e9b7ab7a205ecd1ac9246a608f6fd8190b4
Nikhil Kartha <nikhil.kartha@lge.com>
2015-04-20 17:28:30 -0700

    Moved around some code
    :Release Notes:
    Moved around some code
    
    :Detailed Notes:
    Moved around some code
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:

--- symtab.js
+++ symtab.js
@@ -1,16 +1,12 @@
 var esprima = require('esprima');
 var escope =  require('escope');
 var assert = require('assert');
-//var typeinfo = require('./typeinfo.js');
+var typeinfo = require('./typeinfo.js');
 var astnode = require('./astnode.js');
 var fs = require('fs'),
 
 filename = process.argv[2];
 content = fs.readFileSync(filename, 'utf-8');
-
-var AST = esprima.parse(content, { tolerant: true, loc: true, range: true });
-var SCOPES = escope.analyze(AST).scopes;
-var program_body = SCOPES[0].block.body;
 
 //Symbol Table to be processed by the type checker
 global.DICT={};    
@@ -19,6 +15,11 @@ global.DICT.RETURN_TYPE = [];
 global.DICT.UPPER="NULL";
 global.current_range = "MODULE_"+filename;  // This is for function scope
 global.current_object = "MODULE_"+filename;   //"ROOTOBJECT";
+
+var AST = esprima.parse(content, { tolerant: true, loc: true, range: true });
+var SCOPES = escope.analyze(AST).scopes;
+var program_body = SCOPES[0].block.body;
+
 for(var i in program_body){
     var body = program_body[i];
 
@@ -33,191 +34,4 @@ for(var i in program_body){
 
 console.log("\nGLOBAL DICT: "+JSON.stringify(global.DICT));
 
-function resolve_var(x, FUNC){
-    //console.log("Resolving variable "+x);
-    //console.log('Found value:' + FUNC.VARNAMES[x]);
-    return FUNC.VARNAMES[x];
-}
-
-function propagate_vars(FUNC, DICT)
-{
-    for (var prop in FUNC.VARNAMES){
-        if(FUNC.VARNAMES.hasOwnProperty(prop)){
-            //console.log(prop);
-            //resolve_var(prop, FUNC)
-            if(prop.indexOf("this.") === 0){
-                // Lookup in UPPER scope
-                var name = prop.split('.')[1];
-                if(DICT[FUNC.UPPER]["SCRATCH"] === undefined){ DICT[FUNC.UPPER]["SCRATCH"] = {} }
-                if(DICT[FUNC.UPPER]["SCRATCH"][name] === undefined){ DICT[FUNC.UPPER]["SCRATCH"][name] = [] }
-                //DICT[FUNC.UPPER]["SCRATCH"][name].push(FUNC.VARNAMES[prop]);
-                var vars = FUNC.VARNAMES[prop]
-                for (var i in vars){
-                    if(vars[i].indexOf("this.") === 0){
-                        //console.log("this.");
-                        DICT[FUNC.UPPER]["SCRATCH"][name].push(vars[i]);
-                    } else{
-                        //console.log("resolve");
-                        var multiple = resolve_var(vars[i], FUNC);
-                        if( typeof(multiple) === "string"){
-                            DICT[FUNC.UPPER]["SCRATCH"][name].push(multiple);
-                        } else{
-                            for (var j in multiple){
-                                DICT[FUNC.UPPER]["SCRATCH"][name].push(multiple[j]);
-                            }
-                        }
-                    }
-                }
-                console.log("DICT FUNC UPPER / SCRATCH  :" + JSON.stringify(DICT[FUNC.UPPER]["SCRATCH"]));
-            }
-        }
-    }
-    //console.log("DICT FUNC UPPER / SCRATCH :" + JSON.stringify(DICT[FUNC.UPPER]["SCRATCH"]));
-}
-
-
-function type_check_func(func, GLOBALDICT)
-{
-    /*
-    { nOpts: [ 'opts', 'this.normalizeHoldPulseConfig' ],
-      'this.holdPulseConfig': [ 'nOpts' ] }
-    */
-
-    var DICT = GLOBALDICT;
-    //console.log(DICT[func].VARNAMES);
-    var FUNC = DICT[func];
-
-    if(FUNC){
-        propagate_vars(FUNC, DICT);
-    }
-
-}
-
-function find_type_in_obj (s, obj, x)
-{
-    /*
-    Look for type string "s" in an object "obj" in global.DICT object.
-    s: type string converted to list
-    obj: eg: OBJ_XXX_XXX, i.e key in DICT
-    x: index for s
-    Note: we dont look for obj in s, always swap appropriately.
-    */
-    console.log("find type");
-
-    var DICT = global.DICT;
-
-    if (x >= s.length){
-        return false;
-    }
-
-    if (s[x] === "Literal"){
-        return true;
-    }
-
-    else if (s[x] === "OBJECT"){
-        // we incr the index to get the key for the object, not needed for ARRAY’s 
-        x = x + 1;
-        //console.log(s[x] + " inO " + DICT[obj]);
-        if (s[x] in DICT[obj]){  // events in
-            console.log("found "+ s[x]);
-            obj = DICT[obj][s[x]]  // obj = OBJ_413_440
-            x = x + 1;
-            find_type_in_obj(s, obj, x);
-        } else{
-            console.log("Failed to find " + s[x] + " in " + JSON.stringify(DICT[obj]));
-            return false;
-        }
-    }
-
-    else if ( s[x] === "ARRAY" ){
-        //console.log(s[x] + " inA " + DICT[obj]);
-        if ( s[x] in DICT[obj] ){
-            //found ARRAY;
-            obj = DICT[obj]["ARRAY"]; // obj = OBJ_414_439
-            x = x + 1;
-            find_type_in_obj(s, obj, x);
-        } else{
-            console.log("Failed to find " + s[x] + " in " + JSON.stringify(DICT[obj]));
-            //console.log("Failed to find" + s + " in " + JSON.stringify(DICT[obj]));
-            return false;
-        }
-
-    }
-
-}
-
-function type_check_object(obj1, obj2, scope){
-    console.log("type check object");
-
-    var var1;
-    var var2;
-
-    if(obj1.indexOf("this." === 0)){
-        var1 = obj1.split('.')[1];
-    }
-
-    if(obj2.indexOf("this." === 0)){
-        var2 = obj2.split('.')[1];
-    }
-
-    if(typeof(scope[var1]) === "object"){
-        // Iterate through a list of types, lookup each in var2
-        for(var i in scope[var1]){
-            var s = scope[var1][i]
-            var found = find_type_in_obj(s.split('.'), scope[var2], 0);
-        }
-    }
-
-
-}
-
-//var obj = "OBJ_328_1829";
-function type_check_scratch(obj, DICT){
-    //console.log(DICT[obj]["SCRATCH"]);
-    var cmp = [];
-    var ci = 0;
-    for(prop in DICT[obj]["SCRATCH"]){
-        if(DICT[obj]["SCRATCH"][prop].length > 1){
-            //console.log(DICT[obj]["SCRATCH"][prop]);
-            for( var i in DICT[obj]["SCRATCH"][prop]){
-                if( DICT[obj]["SCRATCH"][prop][i].indexOf("this.") === 0 ){
-                    cmp[ci] = DICT[obj]["SCRATCH"][prop][i];
-                    //console.log(cmp[ci]);
-                    ci = ci + 1;
-                }
-                if( ci === 2 ){
-                    //console.log("find "+cmp);
-                    //find_type_in_obj(cmp[0], cmp[1]);
-                    var scope = DICT[obj];
-                    type_check_object(cmp[0], cmp[1], scope);
-                }
-            }
-        }
-    }
-}
-
-function typechecker(){
-
-    console.log("\nTYPE CHECKING ...\n");
-
-    for(var i in SCOPES){
-        if(i === "0"){
-            console.log("SKIP SCOPE[0]");
-            continue;
-            }
-        var FUNC = "FUNC_"+SCOPES[i].block.body.range[0]+"_"+SCOPES[i].block.body.range[1]
-        console.log("\nProcessing "+ FUNC);
-        type_check_func(FUNC, global.DICT);
-
-    }
-
-    for(var key in global.DICT){
-        if(key.indexOf("OBJ_") === 0){
-            console.log("\nProcessing "+key);
-            type_check_scratch(key,global.DICT);
-        }
-    }
-
-}
-
-typechecker();
+typeinfo.typechecker(SCOPES);
--- typeinfo.js
+++ typeinfo.js
@@ -1,110 +1,189 @@
-extract_type_from_objectexp = function (node, properties, type, type_set){
-    // properties: ObjectExpression.properties array
-    // type: Initial type string, pass in "OBJECT"
-    // type: Array updated with type info
-
-    // Process the properties array
-    //context.report(node,"\n\n++++++++ EXTRACT TYPE  FROM OBJECTEXP++++++:"+JSON.stringify(properties));
-
-    var default_type = type;
-    for (var i in properties){
-        var object = properties[i];
-
-        if (object.key && object.key.type==="Identifier"){
-            type= type + "." + object.key.name
-        }
-
-        if (object.value && object.value.type==="Literal"){
-            // this is the leaf, we got a full type
-            type = type + "." + "Literal";
-            type_set.push(type);
-            //type = default_type; // reset type
-        } 
-        else if (object.value && object.value.type==="ObjectExpression"){
-            type = type + "." + "OBJECT";
-            extract_type_from_objectexp(node, object.value.properties, type, type_set);
-        } 
-        else if (object.value && object.value.type==="ArrayExpression"){
-            type = type + "." + "ARRAY";
-            extract_type_from_arrayexp(node, object.value.elements, type, type_set);
-        }
-        type = default_type; //reset type
-    }
+function resolve_var(x, FUNC){
+    //console.log("Resolving variable "+x);
+    //console.log('Found value:' + FUNC.VARNAMES[x]);
+    return FUNC.VARNAMES[x];
 }
 
-extract_type_from_arrayexp = function (node, elements, type, type_set){
-    // elements: ArrayExpression.elements array
-    // type: Initial type string, pass in "ARRAY"
-    // type_set: Array updated with type info
-    
-    // Process the elements array
-    //context.report(node,"\n\n++++++++ EXTRACT TYPE ++++++ FROM ARRAYEXP:"+JSON.stringify(elements));
-    
-    var default_type = type;
-    for (var i in elements){
-        var object = elements[i];
-
-        if (object.type==="Literal"){
-            // this is the leaf, we got a full type
-            type = type + "." + "Literal";
-            type_set.push(type);
-            //type = default_type; //reset type
-        } 
-        else if (object.type==="ObjectExpression"){
-            type = type + "." + "OBJECT";
-            extract_type_from_objectexp(node, object.properties, type, type_set);
-        } 
-        else if (object.type==="ArrayExpression"){
-            type = type + "." + "ARRAY";
-            extract_type_from_arrayexp(node, object.elements, type, type_set);
-        }
-        type = default_type; //reset type
-    }
-
-}
-
-is_subset = function (arr1, arr2){
-    // Does a suffix match 
-    // Returns a list of type/strings that dont match
-    // Returns an empty string otherwise
-    
-    var msg="";
-    
-    for(var i in arr1){
-        var str1=arr1[i];
-        var found = false;
-
-        /*
-        // Do exact matches
-        if (arr2.indexOf(str1) === -1){
-            msg = msg + str1 + ",";
-        }
-        */
-
-        // Do suffix matches
-        for(var j in arr2){
-            var str2=arr2[j];
-            if(str2.lastIndexOf(str1) !== -1){
-                //msg = msg + str1 + ",";
-                found = true;
+function propagate_vars(FUNC, DICT)
+{
+    for (var prop in FUNC.VARNAMES){
+        if(FUNC.VARNAMES.hasOwnProperty(prop)){
+            //console.log(prop);
+            //resolve_var(prop, FUNC)
+            if(prop.indexOf("this.") === 0){
+                // Lookup in UPPER scope
+                var name = prop.split('.')[1];
+                if(DICT[FUNC.UPPER]["SCRATCH"] === undefined){ DICT[FUNC.UPPER]["SCRATCH"] = {} }
+                if(DICT[FUNC.UPPER]["SCRATCH"][name] === undefined){ DICT[FUNC.UPPER]["SCRATCH"][name] = [] }
+                //DICT[FUNC.UPPER]["SCRATCH"][name].push(FUNC.VARNAMES[prop]);
+                var vars = FUNC.VARNAMES[prop]
+                for (var i in vars){
+                    if(vars[i].indexOf("this.") === 0){
+                        //console.log("this.");
+                        DICT[FUNC.UPPER]["SCRATCH"][name].push(vars[i]);
+                    } else{
+                        //console.log("resolve");
+                        var multiple = resolve_var(vars[i], FUNC);
+                        if( typeof(multiple) === "string"){
+                            DICT[FUNC.UPPER]["SCRATCH"][name].push(multiple);
+                        } else{
+                            for (var j in multiple){
+                                DICT[FUNC.UPPER]["SCRATCH"][name].push(multiple[j]);
+                            }
+                        }
+                    }
+                }
+                console.log("DICT FUNC UPPER / SCRATCH  :" + JSON.stringify(DICT[FUNC.UPPER]["SCRATCH"]));
             }
         }
-
-        if( found === false){
-            msg = msg + str1 + ",";
-        }
     }
-    
-    if(msg){
-        console.log("Types not found: "+msg);
-        console.log("In Object: "+arr2);
-        return msg;
-    }
+    //console.log("DICT FUNC UPPER / SCRATCH :" + JSON.stringify(DICT[FUNC.UPPER]["SCRATCH"]));
+}
 
-    return msg;
+
+function type_check_func(func, GLOBALDICT)
+{
+    /*
+    { nOpts: [ 'opts', 'this.normalizeHoldPulseConfig' ],
+      'this.holdPulseConfig': [ 'nOpts' ] }
+    */
+
+    var DICT = GLOBALDICT;
+    //console.log(DICT[func].VARNAMES);
+    var FUNC = DICT[func];
+
+    if(FUNC){
+        propagate_vars(FUNC, DICT);
+    }
 
 }
 
-exports.objectexp = extract_type_from_objectexp;
-exports.arrayexp = extract_type_from_arrayexp;
-exports.is_subset = is_subset;
+function find_type_in_obj (s, obj, x)
+{
+    /*
+    Look for type string "s" in an object "obj" in global.DICT object.
+    s: type string converted to list
+    obj: eg: OBJ_XXX_XXX, i.e key in DICT
+    x: index for s
+    Note: we dont look for obj in s, always swap appropriately.
+    */
+    console.log("find type");
+
+    var DICT = global.DICT;
+
+    if (x >= s.length){
+        return false;
+    }
+
+    if (s[x] === "Literal"){
+        return true;
+    }
+
+    else if (s[x] === "OBJECT"){
+        // we incr the index to get the key for the object, not needed for ARRAY’s 
+        x = x + 1;
+        //console.log(s[x] + " inO " + DICT[obj]);
+        if (s[x] in DICT[obj]){  // events in
+            console.log("found "+ s[x]);
+            obj = DICT[obj][s[x]]  // obj = OBJ_413_440
+            x = x + 1;
+            find_type_in_obj(s, obj, x);
+        } else{
+            console.log("Failed to find " + s[x] + " in " + JSON.stringify(DICT[obj]));
+            return false;
+        }
+    }
+
+    else if ( s[x] === "ARRAY" ){
+        //console.log(s[x] + " inA " + DICT[obj]);
+        if ( s[x] in DICT[obj] ){
+            //found ARRAY;
+            obj = DICT[obj]["ARRAY"]; // obj = OBJ_414_439
+            x = x + 1;
+            find_type_in_obj(s, obj, x);
+        } else{
+            console.log("Failed to find " + s[x] + " in " + JSON.stringify(DICT[obj]));
+            //console.log("Failed to find" + s + " in " + JSON.stringify(DICT[obj]));
+            return false;
+        }
+
+    }
+
+}
+
+function type_check_object(obj1, obj2, scope){
+    console.log("type check object");
+
+    var var1;
+    var var2;
+
+    if(obj1.indexOf("this." === 0)){
+        var1 = obj1.split('.')[1];
+    }
+
+    if(obj2.indexOf("this." === 0)){
+        var2 = obj2.split('.')[1];
+    }
+
+    if(typeof(scope[var1]) === "object"){
+        // Iterate through a list of types, lookup each in var2
+        for(var i in scope[var1]){
+            var s = scope[var1][i]
+            var found = find_type_in_obj(s.split('.'), scope[var2], 0);
+        }
+    }
+
+
+}
+
+//var obj = "OBJ_328_1829";
+function type_check_scratch(obj, DICT){
+    //console.log(DICT[obj]["SCRATCH"]);
+    var cmp = [];
+    var ci = 0;
+    for(prop in DICT[obj]["SCRATCH"]){
+        if(DICT[obj]["SCRATCH"][prop].length > 1){
+            //console.log(DICT[obj]["SCRATCH"][prop]);
+            for( var i in DICT[obj]["SCRATCH"][prop]){
+                if( DICT[obj]["SCRATCH"][prop][i].indexOf("this.") === 0 ){
+                    cmp[ci] = DICT[obj]["SCRATCH"][prop][i];
+                    //console.log(cmp[ci]);
+                    ci = ci + 1;
+                }
+                if( ci === 2 ){
+                    //console.log("find "+cmp);
+                    //find_type_in_obj(cmp[0], cmp[1]);
+                    var scope = DICT[obj];
+                    type_check_object(cmp[0], cmp[1], scope);
+                }
+            }
+        }
+    }
+}
+
+function typechecker(SCOPES){
+
+    console.log("\nTYPE CHECKING ...\n");
+
+    for(var i in SCOPES){
+        if(i === "0"){
+            console.log("SKIP SCOPE[0]");
+            continue;
+            }
+        var FUNC = "FUNC_"+SCOPES[i].block.body.range[0]+"_"+SCOPES[i].block.body.range[1]
+        console.log("\nProcessing "+ FUNC);
+        type_check_func(FUNC, global.DICT);
+
+    }
+
+    for(var key in global.DICT){
+        if(key.indexOf("OBJ_") === 0){
+            console.log("\nProcessing "+key);
+            type_check_scratch(key,global.DICT);
+        }
+    }
+
+}
+
+
+exports.typechecker = typechecker;
